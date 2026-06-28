21c6ff467063d665f5d5ee6b3651b950cae9df2c
fasciola <faisalmousa86@gmail.com>
2026-06-28 21:56:18 +0400

    Load transparent hero image layout

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './floating-islands.css';
+import './hero-transparent-image.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
